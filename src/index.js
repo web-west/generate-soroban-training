427a6941fa4f599ea9675b11be5c0dfafe3260bb
web.west <web.west5@gmail.com>
2019-06-08 00:08:25 +0300

    Fix debug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SorobanTraining from './SorobanTraining';
 
-const config = {
+/* const config = {
     digit: 1,
     allowedNumbers: [0, 1, 2, 3, 4, 5, 6, 7, 8],
     actions: 5,
@@ -12,12 +12,12 @@ const config = {
         'sub sum != zero': null,
         'actions': ['8-4', '7-4', '6-4', '5-4', '1+4', '2+4', '3+4', '4+4']
     }
-}
+} */
 
-let soromban = new SorobanTraining(config);
+// let soromban = new SorobanTraining(config);
 
 // console.dir(soromban.getRundomCombinationNumbersJoined());
 // console.dir(soromban.getExamplesArray());
-console.dir(soromban.getExamplesString());
+// console.dir(soromban.getExamplesString());
 
 module.exports = SorobanTraining
